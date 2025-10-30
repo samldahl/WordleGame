3e1f270ab77d10689de77baef891f8e16560e276
Sam Dahl <samldahl@gmail.com>
2025-10-30 17:11:12 -0500

    update guess box, handle submission, provide feedback

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const word = 'bread';
 const keys = document.querySelectorAll('.keys');
 const wordLength = 5;
+const guessbox = document.querySelectorAll('.guessbox');
 
 /*---------------------------- Variables (state) ----------------------------*/
  let board = ['','','','','','','','','','','','','','','','','','','','','','','','','',];
@@ -9,19 +10,59 @@ const wordLength = 5;
  let guesses = [];
  let timer = 60;
  let clicks = 0;
+ let currentRow = 1
 
 
-/*-------------------------------- Functions --------------------------------*/
-keys.forEach((key, index) => {
-    key.addEventListener('click', () => {
-        if (guess.length < wordLength) {
-            guess.push(key.textContent);
-            clicks = clicks + 1;
-            console.log(clicks);
-            console.log(guess);
+ /*----------------------------- Event Listeners -----------------------------*/
+ keys.forEach((key, index) => {
+   key.addEventListener('click', () => {
+     if (guess.length < wordLength) {
+       guess.push(key.textContent);
+       clicks = clicks + 1;
+       console.log(clicks);
+       console.log(guess);
+       updateGuessbox();
+       handleGuessSubmission();
+       provideFeedback();
+       
+       // updating guess box & could be its own function
+      }
+    })
+  });
+  /*-------------------------------- Functions --------------------------------*/
+  function updateGuessbox() {
+  guess.forEach((letter, index) => {
+    guessbox[index].textContent = letter;
+    console.log('test');
+   })
+  };
+
+
+  function handleGuessSubmission() {
+    if (guess.length === wordLength) { // 5
+        if (guess.join('') === word) { // combos the array to word
+            console.log('Correct guess!'); 
+        } else {
+            console.log('Incorrect guess!');
         }
-})
-});
+        // Move to the next row
+        guesses.push(guess);
+        guess = [];
+        currentRow++;
+    }
+}
+
+function provideFeedback() {
+    guess.forEach((letter, index) => {
+        if (letter === word[index]) {
+            guessbox[index].style.backgroundColor = 'green';
+        } else if (word.includes(letter)) {
+            guessbox[index].style.backgroundColor = 'yellow';
+        } else {
+            guessbox[index].style.backgroundColor = 'gray';
+        }
+    });
+}
 
 // button_element.forEach((button, index) => {
 //     button.addEventListener('click', () => {
@@ -38,7 +79,10 @@ keys.forEach((key, index) => {
 
 //     })
 //     });
-  /*----------------------------- Event Listeners -----------------------------*/
 /*-------------------------------- Chug --------------------------------*/
  // draw check (only if no winner)
 // Noting for Oct 27th I spent 45 minutes trying to figure out why my keys weren't appearing turns out I just didn't have the word defer when my JS at the top of the page and things weren't loading in time.
+
+// Edge cases are going to be intense
+// double letters in words....
+// 
